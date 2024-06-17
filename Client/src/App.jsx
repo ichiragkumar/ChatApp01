6f25ba90ca387326b55a1789652c03816bf37b0d
ichiragkumar <chiragroy3030@gmail.com>
2024-06-18 01:56:14 +0530

    Donw with two person chatting

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,17 +1,28 @@
 import {io} from "socket.io-client"
 import  {useEffect, useMemo, useState} from "react"
-import {Container, TextField, Typography, Button} from "@mui/material"
+import {Container, TextField, Typography, Button, Box, Stack} from "@mui/material"
 const App = () => {
 
   const socket = useMemo(()=>io("http://localhost:3000"),[])
 
   const [message, setMessage] = useState("")
+  const [room, setRoom] = useState("")
+  const [socketID, setSocketID] = useState("")
+  const [allmessages, setMessagesonScreen] = useState([])
+
+  console.log(message);
+
 
   const handleSubmit = (e)=>{
     e.preventDefault()
     socket.emit("message",message)
     setMessage("")
-  
+  }
+
+  const roomSubmit = (e)=>{
+    e.preventDefault()
+    socket.emit("message",{message,room})
+    setMessage("")
   }
 
 
@@ -19,6 +30,7 @@ const App = () => {
   useEffect(()=>{
 
     socket.on("connect", ()=>{
+      setSocketID(socket.id)
       console.log("connected from client side", socket.id);
     })
 
@@ -32,7 +44,10 @@ const App = () => {
 
     socket.on("message-recieve", (ms)=>{
       console.log(`user ${socket.id} Message Receive ${ms}`);
+      setMessagesonScreen((allmessages) => [...allmessages, ms])
     })
+
+
 
     
     return () =>{
@@ -44,20 +59,37 @@ const App = () => {
   
 
   return <Container maxWidth="sm">
+    <Box sx={{height:50}}/>
 
-    <Typography variant="h1" component="div" gutterBottom>
-          Welcome to SocketIO
-    </Typography>
-    <form onSubmit={handleSubmit}>
+    <Typography variant="h2" component="div" gutterBottom>REAL TIME</Typography>
+    <Typography variant="h5" component="div" gutterBottom>{socketID}</Typography>
+    <form onSubmit={roomSubmit}>
+    <TextField 
+            id="outlined-basic" 
+            label="Enter Room Code" 
+            variant="outlined"  
+            value={room} 
+            onChange={(e)=>{setRoom(e.target.value)
+        }}/>
+      <br/>
      <TextField 
         id="outlined-basic" 
-        label="Outlined" 
+        label="Enter Message" 
         variant="outlined"  
         value={message} 
         onChange={(e)=>{setMessage(e.target.value)
      }}/>
+    <br/>
+    
      <Button type="submit" variant="outlined">Send message</Button>
     </form>
+
+     <Stack>
+      {
+        allmessages.map((eachMessage, index) => (
+          <Typography key={index} variant="h6" component="div" gutterBottom>{eachMessage}</Typography>
+      ))}
+      </Stack> 
   </Container>
 }
 
